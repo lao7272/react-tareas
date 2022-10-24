95d953d7c1aa616aa124064d809b6970ca18424c
Lao <estanislaolanus@gmail.com>
2022-10-24 08:45:58 -0300

    Tarea de express

--- tarea-2/main.js
+++ tarea-2/main.js
@@ -1,13 +1,17 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const PORT = 8080;
+let arrProd = [];
 //Clase Producto
 class Product {
-    constructor(id, name, brand){
+    constructor(id, name, brand, price){
         this.id = id;
         this.name = name;
         this.brand = brand;
+        this.price = price
     }
 }
-const fs = require('fs');
-let arrProd = [];
 
 // Funciones de FS
 const save = (object) => {
@@ -45,6 +49,7 @@ const deleteById = (id) => {
     .then(data => {
         const arrProduct = JSON.parse(data);
         const newProdArr = arrProduct.filter(prod => prod.id !== id);
+        arrProd = newProdArr;
         fs.writeFile("./products.json", JSON.stringify(newProdArr), 'utf-8', err => {
             if (err) {
                 console.log(err);
@@ -63,21 +68,47 @@ const deleteAll = () => {
 };
 
 
+
 // Ejecucion de las funciones
 
-const product1 = new Product(1, 'zapatilla', 'converse');
-const product2 = new Product(2, 'zapatilla', 'nike');
-const product3 = new Product(3, 'zapatilla', 'adidas');
-const product4 = new Product(4, 'zapatilla', 'puma');
+const product1 = new Product(1, 'zapatilla', 'converse', 20000);
+const product2 = new Product(2, 'zapatilla', 'nike', 23000);
+const product3 = new Product(3, 'zapatilla', 'adidas', 17000);
+const product4 = new Product(4, 'zapatilla', 'puma', 18000);
 
 // save(product1);
 // save(product2);
 // save(product3);
 // save(product4);
 
-// getById(1);
+// getById(4);
 // getAll();
 // deleteById(1)
 // deleteAll();
 
 
+app.get('/', (req, res) =>{
+    res.send({message:`Funciona ${PORT}`})
+});
+
+app.get('/productos', (req, res) =>{
+    const writeProds = async () => {
+        const arrProducts = await getAll();
+        res.send(arrProducts);
+    }
+    writeProds();
+});
+
+app.get('/productoRandom', (req, res) =>{
+    
+    const writeProd = async () => {
+        const arrProducts = await getAll();
+        res.send(arrProducts[Math.floor(Math.random() * arrProducts.length)]);
+    }
+    writeProd()
+});
+
+
+const server = app.listen(PORT, ()=> {
+    console.log(`Server con puerto ${server.address().port} funcionando`)
+});
